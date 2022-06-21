aa8c2025dacacb04b0882a4dddcd7f09ecde9526
David Ayomide Olaniyi <63981998+Dhavisco@users.noreply.github.com>
2022-06-21 09:26:25 +0100

    script

---
+++ script.js
@@ -0,0 +1,9 @@
+
+//Declare Variables
+var user_name, user_age, user_country;
+user_name = "Olaniyi David";
+user_age = 20;
+user_country = "Nigeria";
+
+//print value
+document.getElementById('print').innerHTML ="My name is " + user_name + ", I am " + user_age + " years old, i am from " + user_country + "." 
